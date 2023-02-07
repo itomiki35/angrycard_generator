cdb4cdf8fdf224ed9ca6963831b31612a367c0e5
itomiki35 <o.miki22@gmail.com>
2023-02-07 16:18:39 +0900

    Add:sorceryの導入、UsersController、UserSessionsController、Usersテーブルの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  get 'logout', to: 'user_sessions#destroy'
+
+  resources :users, only: %i[new create]
 end
