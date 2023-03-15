9d3ffb33a96b4d506e098bc67d777fecae83a2ac
itomiki35 <o.miki22@gmail.com>
2023-03-15 10:45:02 +0900

    Add:AngryCardsController、Viewの追加

---
+++ app/controllers/angry_cards_controller.rb
@@ -0,0 +1,44 @@
+class AngryCardsController < ApplicationController
+  def index
+    @angry_cards = AngryCard.all
+  end
+
+  def new
+    @angry_card = AngryCard.new
+  end
+
+  def create
+    @angry_card = AngryCard.new(angry_card_params)
+      @angry_card.create_image
+      @angry_card.generate_image
+      if @angry_card.save
+      
+      # @angry_card.generate_image
+
+       #@angry_card.save
+       # トランザクション
+
+        redirect_to angry_card_path(@angry_card.id)
+      else
+        render :new
+      end
+  end
+
+  def show
+    @angry_card = AngryCard.find(params[:id])
+  end
+
+  def destroy
+    @angry_card = AngryCard.find(params[:id])
+    @angry_card.destroy
+    redirect_to angry_cards_path
+  end
+
+
+  private
+
+  def angry_card_params
+    params.require(:angry_card).permit(:title, :body)
+  end
+
+end
