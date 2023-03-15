fba8a8a821c3be572ccf9b43a7f7eaca3c0f1644
itomiki35 <o.miki22@gmail.com>
2023-03-15 13:49:58 +0900

    Fix:session関連の修正

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -5,9 +5,16 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_back_or_to root_path
+      redirect_to root_path
     else
       render :new
     end
   end
+  
+  def destroy
+    logout
+    #flash[:success] = t('.success')
+    redirect_to root_path
+  end
+
 end
