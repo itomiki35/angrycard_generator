16a0138f21ff0e350a76a82782c68afea8a6d002
itomiki35 <o.miki22@gmail.com>
2023-03-15 10:43:19 +0900

    Userモデルの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_07_070803) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_15_012236) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "angry_cards", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "body", null: false
+    t.string "image"
+    t.string "generated_card"
+    t.integer "level"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_angry_cards_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -25,4 +37,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_070803) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "angry_cards", "users"
 end
