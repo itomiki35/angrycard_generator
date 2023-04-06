96565c38838e8b6cc3d66f59017357f13afd73a2
itomiki35 <o.miki22@gmail.com>
2023-04-06 14:55:51 +0900

    Add Fix:マイページの追加(途中)、destroyアクションの修正_#36

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[new create]
   resources :angry_cards, only: %i[new create index show destroy]
+  resources :my_pages, only: %i[show edit]
 
 end
