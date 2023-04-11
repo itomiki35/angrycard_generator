04ef85a9b8729784b7f3da347ae3e5d98ff2c29d
itomiki35 <o.miki22@gmail.com>
2023-04-11 19:58:04 +0900

    Add:meta-tagsの設定 #54

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'アングリーカードジェネレーター',
+      title: '俺のターン！ドロー！ストレスをアングリーカードとして召喚！クリックしてアングリーカードを召喚しよう！',
+      reverse: true,
+      separator: '|',
+      description: '俺のターン！ドロー！ストレスをアングリーカードとして召喚！日常のちょっとしたストレスを、カードゲーム風に遊び心を持って昇華させるサービスです。',
+      keywords: 'アングリーカードジェネレーター',
+      charset: 'UTF-8',
+      canonical: 'https://www.angrycard-generator.com',
+      noindex: !Rails.env.production?,
+      icon: [
+        { href: image_url('favicon.ico'), sizes: '32x32' }
+      ],
+      og: {
+        site_name: 'アングリーカードジェネレーター',
+        title: '俺のターン！ドロー！ストレスをアングリーカードとして召喚！クリックしてアングリーカードを召喚しよう！',
+        description: '俺のターン！ドロー！ストレスをアングリーカードとして召喚！日常のちょっとしたストレスを、カードゲーム風に遊び心を持って昇華させるサービスです。',
+        type: 'website',
+        url: 'https://www.angrycard-generator.com',
+        image: image_url('ogp.png'),
+        locale: 'ja_JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@itomiki_hiyoko'
+      }
+    }
+  end
 end
