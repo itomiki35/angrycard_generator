6678d27f4a110a465f39e560dafda8d06bf362a6
itomiki35 <o.miki22@gmail.com>
2023-04-19 15:49:48 +0900

    fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'logout', to: 'user_sessions#destroy'
   get 'guest_login', to: 'user_sessions#guest_login'
 
+  get 'sitemap', to: redirect("https://s3-ap-northeast-1.amazonaws.com/#{ENV['S3_BUCKET_NAME']}/sitemaps/sitemap.xml.gz")
 
   resources :users, only: %i[new create]
   resources :angry_cards, only: %i[new create index show destroy]
